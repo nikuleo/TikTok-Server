e0ecc22d5775599b03bbfaed1c60520938a6a142
niku <nikuliu@foxmail.com>
2023-06-05 15:29:54 +0800

    ✨ feat: Implement Gin router logger middleware

---
+++ middleware/logger.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"TikTokServer/app/log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GinLog() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		start := time.Now()
+		path := ctx.Request.URL.Path
+		query := ctx.Request.URL.RawQuery
+		status := ctx.Writer.Status()
+		clientIP := ctx.ClientIP()
+		method := ctx.Request.Method
+		ctx.Next()
+		userAgent := ctx.Request.UserAgent()
+		duration := time.Since(start)
+		log.Info(
+			"GIN",
+			log.String("IP", clientIP),
+			log.String("path", path),
+			log.String("method", method),
+			log.Int("status", status),
+			log.String("query", query),
+			log.String("userAgent", userAgent),
+			log.String("error", ctx.Errors.ByType(gin.ErrorTypePrivate).String()),
+			log.Duration("duration", duration),
+		)
+	}
+}
