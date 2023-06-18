50984b077e5c196a28bf51251a5bd614280779b6
niku <nikuliu@foxmail.com>
2023-06-18 20:15:12 +0800

    ✨ feat: Add config.go read project config file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,7 +1,31 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"TikTokServer/pkg/tlog"
+
+	"github.com/spf13/viper"
+)
 
 type Config struct {
 	Viper *viper.Viper
+}
+
+func GetConfig(cfgName string) Config {
+	v := viper.New()
+	cfg := Config{
+		Viper: v,
+	}
+
+	viper := cfg.Viper
+
+	viper.SetConfigName(cfgName)
+	viper.AddConfigPath(".config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		tlog.Fatalf("reading config: %s, failed: %s", cfgName, err.Error())
+	}
+
+	tlog.Infof("reading config: %s, success.", viper.ConfigFileUsed())
+
+	return cfg
 }
