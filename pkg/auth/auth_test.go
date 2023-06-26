a19c41cd475be4bfef9e2ff000472a7d3de8955b
niku <nikuliu@foxmail.com>
2023-06-26 13:34:18 +0800

    ✨ feat: add  auth package for JWT authentication

---
+++ pkg/auth/auth_test.go
@@ -0,0 +1,35 @@
+package auth
+
+import (
+	"TikTokServer/pkg/tlog"
+	"fmt"
+	"testing"
+)
+
+func init() {
+	tlog.InitLog()
+	InitJWT()
+}
+
+func TestInitJWT(t *testing.T) {
+	fmt.Println(secret, expireHour)
+}
+
+func TestCreateToken(t *testing.T) {
+	token, err := CreateToken(2233, "niku")
+	fmt.Println(token, err)
+}
+
+func TestValidateToken(t *testing.T) {
+	token, err := CreateToken(2233, "niku")
+	fmt.Println(token, err)
+	isValidated, err := ValidateToken(token)
+	fmt.Println(isValidated, err)
+}
+
+func TestGetUserIDByToken(t *testing.T) {
+	token, err := CreateToken(2233, "niku")
+	fmt.Println("token:", token, err)
+	userID, err := GetUserIDByToken(token)
+	fmt.Println("userID:", userID, err)
+}
