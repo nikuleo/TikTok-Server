44260051da68e6d022661de7a8ea252fe7628d6f
niku <nikuliu@foxmail.com>
2023-06-29 15:33:39 +0800

    ✨ feat: 路由分配鉴权中间件

--- routes/favorite.go
+++ routes/favorite.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"TikTokServer/controller"
+	"TikTokServer/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func addFavoriteRoutes(rg *gin.RouterGroup) {
 	favorite := rg.Group("/favorite")
-
+	favorite.Use(middleware.JwtAuthMiddleware())
 	favorite.POST("/action/", controller.FavoriteAction)
 	favorite.GET("/list/", controller.GetFavoriteList)
 }
