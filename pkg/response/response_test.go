a2aa6c540d5cb8a042ea313faf5d57c8f59a6b1d
niku <nikuliu@foxmail.com>
2023-06-29 15:37:14 +0800

    🐞 fix: 更改响应包装方式，使用反射修改指针类型内容

---
+++ pkg/response/response_test.go
@@ -0,0 +1,27 @@
+package response
+
+import (
+	message "TikTokServer/idl/gen"
+	"TikTokServer/pkg/errorcode"
+	"errors"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestSuccess(t *testing.T) {
+	w := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(w)
+
+	he := errorcode.HttpSuccess
+	dbErr := errorcode.ErrHttpDatabase
+	dbErr.SetError(errors.New("db error"))
+	Success(ctx, nil, nil)
+	Success(ctx, he, &message.DouyinUserResponse{
+		StatusCode: int32(he.ErrCode),
+		StatusMsg:  nil,
+		User:       nil,
+	})
+	Success(ctx, dbErr, nil)
+}
