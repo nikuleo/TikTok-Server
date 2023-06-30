6605e55719dc8c4409ced0c834b6d34c08997288
niku <nikuliu@foxmail.com>
2023-06-30 22:27:25 +0800

    🐞 fix: 修改全局变量初始化bug := 改为 =

--- model/init.go
+++ model/init.go
@@ -15,10 +15,11 @@ import (
 )
 
 var (
-	DB *gorm.DB
+	db *gorm.DB
 )
 
 func InitDB() {
+	var err error
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
@@ -43,7 +44,8 @@ func InitDB() {
 		viper.GetBool("mysql.parseTime"),
 		viper.GetString("mysql.loc"),
 	)
-	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+	// 注意使用 = 赋值，而不是 := 赋值，否则会覆盖全局变量 db，全局 db 仍然为 nil
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: newLogger,
 	})
 
@@ -52,12 +54,12 @@ func InitDB() {
 	}
 
 	// 自动迁移, 若模型在数据库中不存在，则会自动创建对应的表，若已存在，则会检查字段是否发生变化，若发生变化，则会修改表结构
-	if err := DB.AutoMigrate(&User{}, &Video{}, &Comment{}, &Relation{}, &Message{}); err != nil {
+	if err := db.AutoMigrate(&User{}, &Video{}, &Comment{}, &Relation{}, &Message{}); err != nil {
 		tlog.Error(err.Error())
 	}
 
 	// 获取通用数据库对象 sql.DB ，然后使用其提供的功能
-	sqlDB, err := DB.DB()
+	sqlDB, err := db.DB()
 	if err != nil {
 		tlog.Error(err.Error())
 	}
@@ -72,9 +74,9 @@ func InitDB() {
 	tlog.Infof("DB init success, user: %s", viper.GetString("mysql.user"))
 }
 
-func GetDB() *gorm.DB {
-	return DB
-}
+// func GetDB() *gorm.DB {
+// 	return db
+// }
 
 // gorm 1.20 版本之后使用连接池，无需显示关闭连接
 // func CloseDB() {
