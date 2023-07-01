8077f51b002a41ee9090a84775a0cb155371aed6
niku <nikuliu@foxmail.com>
2023-07-01 13:03:19 +0800

    ✨ feat: 实现用户注册、登录、用户信息业务逻辑

--- controller/userController.go
+++ controller/userController.go
@@ -1,22 +1,69 @@
 package controller
 
 import (
+	"TikTokServer/pkg/errorcode"
+	"TikTokServer/pkg/response"
 	"TikTokServer/pkg/tlog"
+	"TikTokServer/service"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UserRegister(ctx *gin.Context) {
-	// TODO: implement
-	tlog.Info("UserRegister")
+
+	userName := ctx.Query("username")
+	password := ctx.Query("password")
+
+	resp, err := service.UserRegister(userName, password)
+
+	if err != nil {
+		tlog.Errorf(err.Error(), resp)
+		response.Fail(ctx, err, nil)
+		return
+	}
+
+	response.Success(ctx, errorcode.HttpSuccess, resp)
+
 }
 
 func UserLogin(ctx *gin.Context) {
-	// TODO: implement
+
 	tlog.Info("Usertlogin")
+
+	userName := ctx.Query("username")
+	password := ctx.Query("password")
+
+	resp, err := service.UserLogin(userName, password)
+
+	if err != nil {
+		tlog.Errorf(err.Error(), resp)
+		response.Fail(ctx, err, nil)
+		return
+	}
+
+	response.Success(ctx, errorcode.HttpSuccess, resp)
 }
 
 func GetUserInfo(ctx *gin.Context) {
-	// TODO: implement
-	tlog.Info("UserInfo")
+
+	userID := ctx.Query("user_id")
+	authID, _ := ctx.Get("userID")
+	tlog.Debugf("userID: %v", userID)
+	tlog.Debugf("authID: %v, %t", authID, authID)
+	aID := authID.(int64)
+
+	if strconv.FormatInt(aID, 10) != userID {
+		response.Fail(ctx, errorcode.ErrHttpTokenInvalid, nil)
+	}
+
+	resp, err := service.GetUserInfo(aID)
+
+	if err != nil {
+		tlog.Errorf(err.Error(), resp)
+		response.Fail(ctx, err, nil)
+		return
+	}
+
+	response.Success(ctx, errorcode.HttpSuccess, resp)
 }
---
+++ model/userModel.go
@@ -0,0 +1,66 @@
+package model
+
+import (
+	"TikTokServer/pkg/tlog"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	UserName        string  `gorm:"type:varchar(32);not null;index:idx_username,unique;"`
+	Password        string  `gorm:"type:varchar(80);not null;"`
+	FavoriteVideo   []Video `gorm:"many2many:user_favorite_videos;"`
+	TotalFavorited  int     `gorm:"default:0;"`
+	WorkCount       int     `gorm:"default:0;"`
+	FollowingCount  int     `gorm:"default:0;"`
+	FollowerCount   int     `gorm:"default:0;"`
+	Avatar          string  `gorm:"type:varchar(255);default:NULL;"`
+	BackgroundImage string  `gorm:"type:varchar(255);default:NULL;"`
+	Signature       string  `gorm:"type:varchar(255);default:NULL;"`
+}
+
+func (User) TableName() string {
+	return "user"
+}
+
+func GetUserByID(userID int64) (*User, error) {
+	user := &User{}
+	result := db.Where("id = ?", userID).Find(&user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	tlog.Infof("User: %v", user)
+	return user, nil
+}
+
+func CreateUser(userName, password string) (*User, error) {
+	user := &User{
+		UserName:        userName,
+		Password:        password,
+		Avatar:          "https://oss.nikunokoya.com/blogbucket/images/others/avatar.png",
+		BackgroundImage: "https://oss.nikunokoya.com/blogbucket/post/MMD-RayCast/RayEffect.png",
+		Signature:       "这个人很懒，什么都没有留下",
+	}
+	result := db.Create(&user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	tlog.Infof("CreateUser success: %v", user)
+	return user, nil
+}
+
+func QuaryUser(userName string) (*User, error) {
+	user := &User{}
+	result := db.Where("user_name = ?", userName).Take(&user)
+	tlog.Debugf("QuaryUser: ", result.Error)
+	tlog.Debugf("user: %v", user)
+	if result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			return nil, nil
+		} else {
+			return nil, result.Error
+		}
+	}
+	return user, nil
+}
