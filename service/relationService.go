6356dd4dc971734f142849ee790bdb8127095d31
niku <nikuliu@foxmail.com>
2023-07-20 23:08:00 +0800

    ✨ feat: 实现某用户的关注信息缓存

--- service/relationService.go
+++ service/relationService.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"TikTokServer/cache"
 	message "TikTokServer/idl/gen"
 	"TikTokServer/model"
 	"TikTokServer/pkg/errorcode"
@@ -22,13 +23,44 @@ func RelationAction(authID, toUserID, actionType int64) (*message.DouyinRelation
 		errCode.SetError(err)
 		return nil, errCode
 	}
-
+	// TODO: 使用消息队列异步删除缓存
 	return &message.DouyinRelationActionResponse{}, nil
 }
 
+// 缓存优化循环查询
 func GetFollowList(userID int64) (*message.DouyinRelationFollowListResponse, error) {
 
-	followUserList, err := model.GetFollowList(userID)
+	// 从缓存中获取关注列表
+	userList, err := getFollowListFromCache(userID)
+	if err != nil {
+		errCode := errorcode.ErrHttpCache
+		errCode.SetError(err)
+		return nil, errCode
+	}
+
+	// 缓存未命中，从数据库中获取关注列表，并写入缓存
+	if userList == nil {
+		userList, err = getFollowListFromDatabase(userID)
+		if err != nil {
+			errCode := errorcode.ErrHttpDatabase
+			errCode.SetError(err)
+			return nil, errCode
+		}
+	}
+
+	cache.SetUserFollowing(userID, userList)
+
+	resp := &message.DouyinRelationFollowListResponse{
+		UserList: userList,
+	}
+
+	return resp, nil
+}
+
+// 通过缓存的 ID 优化循环查询
+func GetFollowListByUserIDs(userIDs []int64) ([]*message.User, error) {
+
+	followUserList, err := model.MutilGetUserByID(userIDs)
 	if err != nil {
 		errCode := errorcode.ErrHttpDatabase
 		errCode.SetError(err)
@@ -38,11 +70,7 @@ func GetFollowList(userID int64) (*message.DouyinRelationFollowListResponse, err
 	for i := range userList {
 		userList[i].IsFollow = true
 	}
-	resp := &message.DouyinRelationFollowListResponse{
-		UserList: userList,
-	}
-
-	return resp, nil
+	return userList, nil
 }
 
 func GetFollowerList(userID int64) (*message.DouyinRelationFollowerListResponse, error) {
@@ -82,4 +110,50 @@ func PackUserList(users []*model.User) []*message.User {
 		}
 	}
 	return userList
+}
+
+func getFollowListFromCache(userID int64) ([]*message.User, error) {
+	followIDList, err := cache.GetUserFollowing(userID)
+	if err != nil {
+		return nil, err
+	}
+	if len(followIDList) == 0 {
+		return nil, nil
+	}
+
+	return GetFollowListByUserIDs(followIDList)
+}
+
+func getFollowListFromDatabase(userID int64) ([]*message.User, error) {
+	followUserList, err := model.GetFollowList(userID)
+	if err != nil {
+		return nil, err
+	}
+
+	userList := PackUserList(followUserList)
+	for i := range userList {
+		userList[i].IsFollow = true
+	}
+
+	return userList, nil
+}
+
+func getFollowUserIDs(userID int64) ([]int64, error) {
+	followIDList, err := cache.GetUserFollowing(userID)
+	if err != nil {
+		return nil, err
+	}
+	if len(followIDList) != 0 {
+
+		return followIDList, nil
+	}
+	userList, err := getFollowListFromDatabase(userID)
+	if err != nil {
+		return nil, err
+	}
+	followIDList = make([]int64, len(userList))
+	for i, u := range userList {
+		followIDList[i] = u.Id
+	}
+	return followIDList, nil
 }
