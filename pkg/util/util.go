c143446e07b780dbc05b71109771d0507604f4c9
niku <nikuliu@foxmail.com>
2023-07-20 23:02:32 +0800

    ✨ feat: util 工具包添加 string 切片转 int64切片函数

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,7 +1,26 @@
 package util
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 func GetCurrentTime() int64 {
 	return time.Now().UnixNano() / 1e6
+}
+
+// 实现 string 切片转 int64 切片工具函数
+func ConvtStrSliceToInt64Slice(strSlice []string) ([]int64, error) {
+	intSlice := make([]int64, 0, len(strSlice))
+
+	for _, str := range strSlice {
+		num, err := strconv.ParseInt(str, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+
+		intSlice = append(intSlice, num)
+	}
+
+	return intSlice, nil
 }
