de07e0f492bcbb01283321a8c64d78abbfb032a3
niku <nikuliu@foxmail.com>
2023-07-24 14:26:05 +0800

    🐞 fix: 修复多次发送点赞请求时多表关联更新 BUG

--- model/favoriteModel.go
+++ model/favoriteModel.go
@@ -6,6 +6,11 @@ import (
 	"gorm.io/gorm"
 )
 
+type UserFavoriteVideos struct {
+	UserID  int64 `gorm:"primaryKey;"`
+	VideoID int64 `gorm:"primaryKey;"`
+}
+
 func Favorite(userID, videoID int64) error {
 	err := db.Transaction(func(tx *gorm.DB) error {
 		user := new(User)
@@ -22,40 +27,45 @@ func Favorite(userID, videoID int64) error {
 		if err := tx.Model(user).Association("FavoriteVideos").Append(video); err != nil {
 			return err
 		}
-		res := tx.Model(video).Update("favorite_count", gorm.Expr("favorite_count + ?", 1))
+		// videoCnt := tx.Model(video).Association("FavoriteVideos").Count()
+		var videoCnt int64
+		tx.Model(&UserFavoriteVideos{}).Where("video_id = ?", videoID).Count(&videoCnt)
+
+		// tlog.Debugf("video: %v, user: %v", video, user)
+		// tlog.Debugf("videoCnt: %v", videoCnt)
+		res := tx.Model(video).Update("favorite_count", videoCnt)
 
 		if res.Error != nil {
 			return res.Error
 		}
-
 		if res.RowsAffected != 1 {
 			errCode := errorcode.ErrHttpDatabase
 			errCode.SetMsg("更新字段数量大于1")
 			return errCode
 		}
 
-		res = tx.Model(user).Update("favorite_count", gorm.Expr("favorite_count + ?", 1))
-		if res.Error != nil {
-			return res.Error
-		}
+		// TODO: 这两个字段重要性不高，可以后续使用定时任务从 redis 缓存里更新
+		// res = tx.Model(user).Update("favorite_count", gorm.Expr("favorite_count + ?", 1))
+		// if res.Error != nil {
+		// 	return res.Error
+		// }
 
-		if res.RowsAffected != 1 {
-			errCode := errorcode.ErrHttpDatabase
-			errCode.SetMsg("更新字段数量大于1")
-			return errCode
-		}
+		// if res.RowsAffected != 1 {
+		// 	errCode := errorcode.ErrHttpDatabase
+		// 	errCode.SetMsg("更新字段数量大于1")
+		// 	return errCode
+		// }
 
-		// 更新视频作者的 TotalFavorited 字段
-		res = tx.Model(User{}).Where("id = ?", video.AuthorID).Update("total_favorited", gorm.Expr("total_favorited + ?", 1))
-		if res.Error != nil {
-			return res.Error
-		}
-
-		if res.RowsAffected != 1 {
-			errCode := errorcode.ErrHttpDatabase
-			errCode.SetMsg("更新字段数量大于1")
-			return errCode
-		}
+		// // 更新视频作者的 TotalFavorited 字段
+		// res = tx.Model(User{}).Where("id = ?", video.AuthorID).Update("total_favorited", gorm.Expr("total_favorited + ?", 1))
+		// if res.Error != nil {
+		// 	return res.Error
+		// }
+		// if res.RowsAffected != 1 {
+		// 	errCode := errorcode.ErrHttpDatabase
+		// 	errCode.SetMsg("更新字段数量大于1")
+		// 	return errCode
+		// }
 
 		return nil
 	})
@@ -80,40 +90,41 @@ func DisFavorite(userID, videoID int64) error {
 			return err
 		}
 
-		res := tx.Model(video).Update("favorite_count", gorm.Expr("favorite_count - ?", 1))
+		var videoCnt int64
+		tx.Model(&UserFavoriteVideos{}).Where("video_id = ?", videoID).Count(&videoCnt)
+		res := tx.Model(video).Update("favorite_count", videoCnt)
 
 		if res.Error != nil {
 			return res.Error
 		}
-
 		if res.RowsAffected != 1 {
 			errCode := errorcode.ErrHttpDatabase
 			errCode.SetError(res.Error)
 			return errCode
 		}
 
-		res = tx.Model(user).Update("favorite_count", gorm.Expr("favorite_count - ?", 1))
-		if res.Error != nil {
-			return res.Error
-		}
+		// res = tx.Model(user).Update("favorite_count", gorm.Expr("favorite_count - ?", 1))
+		// if res.Error != nil {
+		// 	return res.Error
+		// }
 
-		if res.RowsAffected != 1 {
-			errCode := errorcode.ErrHttpDatabase
-			errCode.SetMsg("更新字段数量大于1")
-			return errCode
-		}
+		// if res.RowsAffected != 1 {
+		// 	errCode := errorcode.ErrHttpDatabase
+		// 	errCode.SetMsg("更新字段数量大于1")
+		// 	return errCode
+		// }
 
-		// 更新视频作者的 TotalFavorited 字段
-		res = tx.Model(User{}).Where("id = ?", video.AuthorID).Update("total_favorited", gorm.Expr("total_favorited - ?", 1))
-		if res.Error != nil {
-			return res.Error
-		}
+		// // 更新视频作者的 TotalFavorited 字段
+		// res = tx.Model(User{}).Where("id = ?", video.AuthorID).Update("total_favorited", gorm.Expr("total_favorited - ?", 1))
+		// if res.Error != nil {
+		// 	return res.Error
+		// }
 
-		if res.RowsAffected != 1 {
-			errCode := errorcode.ErrHttpDatabase
-			errCode.SetMsg("更新字段数量大于1")
-			return errCode
-		}
+		// if res.RowsAffected != 1 {
+		// 	errCode := errorcode.ErrHttpDatabase
+		// 	errCode.SetMsg("更新字段数量大于1")
+		// 	return errCode
+		// }
 
 		return nil
 	})
